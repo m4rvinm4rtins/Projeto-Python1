fd604cfeccc5146b790519c610a1becb7ecf9c43
Marvin Martins <none>
2023-01-06 23:14:23 -0300

    Definindo as demais variaveis e plotando o gráfico.

--- Arquivo/projeto.py
+++ Arquivo/projeto.py
@@ -2,4 +2,10 @@ import matplotlib.pyplot as fig
 import datetime as dt
 import pandas_datareader.data as web
 
+
 inicio = dt.datetime(2019,1,1)
+fim = dt.datetime(2019,11,24)
+df = web.DataReader('PETR4.SA','yahoo',inicio,fim)
+df['Close'].plot(color='k',lw=4)
+fig.grid()
+fig.title('PETR4 (Jan-Nov / 2019)',fontsize=18,weight='bold')
